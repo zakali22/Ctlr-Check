4a4f6fc3c847941912313ea656f21030fe2e46a8
Zakeria Ali <zak.fined@gmail.com>
2017-07-24 16:20:37 +0100

    Add the delete and add functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  var text = $('.type input');
+  var add = $('.btn-success');
+  var remove = $('.btn-danger');
+
+  add.on('click', function() {
+    /* Act on the event */
+    var value = text.val();
+    console.log(value);
+
+    var output = '<div class="item">';
+    output += '<input type="checkbox">';
+    output += '<p>' + value + '</p>';
+    output += '</div>';
+
+    $('.container').append(output);
+  });
+
+  remove.on('click', function() {
+    var list = $('.item input');
+    console.log(list);
+    $.each(list, function(index, item) {
+      if (item.checked) {
+        item.closest('div').remove();
+      }
+    });
+  });
+
+});
